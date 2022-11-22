bff0c8efa1d83eb147fdaf6463fbce36b0a8a7ad
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-22 16:03:56 +0800

    Add files via upload

---
+++ days43_fungsi.py
@@ -0,0 +1,8 @@
+#list menggunakan fungsi
+
+def angka (list_angka):
+    angka_angka = list_angka.copy()
+    for number in angka_angka:
+        print("angka yang ->",number)
+lis=["pertama","kedua","ketiga"]
+angka (lis)
